5495e6e43b6ae58b7bf6862ec3d721aae14067e6
DESKTOP-KTQDT91\Safi <shasani0451@gmail.com>
2021-03-14 00:36:10 -0500

    Implemented ball basics

---
+++ Break the Line!/Assets/ballMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+
+    public GameObject paddle;
+    bool start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!start)
+        {
+            this.transform.position = paddle.transform.position + new Vector3(2f, 0f, 0f); 
+        }
+    }
+}
