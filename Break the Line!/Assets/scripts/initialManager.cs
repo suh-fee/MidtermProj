6e67dcd7641ae1f76a06f0138fd521d4fb9397e8
DESKTOP-KTQDT91\Safi <shasani0451@gmail.com>
2021-03-14 23:35:23 -0400

    final commit!

---
+++ Break the Line!/Assets/scripts/initialManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class initialManager : MonoBehaviour
+{
+    GUIStyle style = new GUIStyle();
+    bool show = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            show = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnGUI()
+    {
+        if (show)
+        {
+            GUI.backgroundColor = Color.yellow;
+            GUI.Label(new Rect(100, 100, Screen.width, Screen.height), "Press space to launch ball \nW and S to move paddle\nR to reset ball", style);
+        }
+        
+    }
+}
